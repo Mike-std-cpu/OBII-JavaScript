85e401d4e3830a3917ccad5ba8f3fe8d989ae5c6
Mike Andrade <hoodloom07@outlook.es>
2022-10-26 23:11:38 -0500

    13.3 more Errors

--- module13/index.js
+++ module13/index.js
@@ -1,5 +1,10 @@
 const name = "Mike";
 
-           var name2 = "Sarai"
+           var name2 = 'Sarai'
 
     console.log(`${name} y ${name2}`)
+
+const onjeto = {
+       nombre : 'mike',
+       
+}
