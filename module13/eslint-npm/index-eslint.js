64837756482fc24ae13ac285f8497c9fb94d35fe
Mike Andrade <hoodloom07@outlook.es>
2022-10-28 00:21:22 -0500

    13.9 npm lint comand add

--- module13/eslint-npm/index-eslint.js
+++ module13/eslint-npm/index-eslint.js
@@ -1,3 +1,7 @@
 const nombre = "Mike"
 //const persona = 'Sara' // Esto marca error por las reglas de ESLint
 const persona = "Sarai"
+
+const string = 'String con comillas simples'
+
+    console.log('hola')
