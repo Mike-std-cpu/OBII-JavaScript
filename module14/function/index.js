eb2b818125e486b39ee429eda3131d70b310f374
Mike Andrade <hoodloom07@outlook.es>
2022-11-04 20:45:11 -0600

    14.7 Events in JS

--- module14/function/index.js
+++ module14/function/index.js
@@ -6,3 +6,11 @@ console.log(`La suma dentro de javascript es: ${suma(6,4)}`);
 
 const p = document.getElementById("parrafo");
 console.log(`Se cacho la etiqueta desde JS: ${p}`);
+
+const form = document.getElementById("Formulario");
+
+//Evento para no hacer ejecucion por defecto
+form.addEventListener("submit",(evento) => { // cada submit realizar evento
+    console.log(evento);// Informacion del evento
+    evento.preventDefault();
+})
