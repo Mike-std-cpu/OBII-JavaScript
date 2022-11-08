d3bee61fefb152cf92be533cc190e5d744511514
Mike Andrade <hoodloom07@outlook.es>
2022-11-07 21:36:56 -0600

    14.4 Events with color over mouse, changes color

--- module14/jquery/index.js
+++ module14/jquery/index.js
@@ -27,6 +27,22 @@ $(() => {
     })
 
     $(".new-element").click(()=>{
-        $("ul").append("<li> New Element</li>")
+        //$("ul").append("<li> New Element in the final</li>")
+        $("ul").prepend("<li> New Element in the start</li>")
     })
+
+    // Pasando el cursor por ensima de los elementos
+    $("h1").mouseenter((Elem)=>{
+        //console.log("Has pasado el rato en el titulo (hover)");
+        console.log("Paso el mouse por el titulo, cambiando a verde...");
+        Elem.target.style.color = "green"
+    })
+
+    // Dejando de estar el cursor por los elementos
+    $("h1").mouseleave((Elem)=>{
+        
+        console.log("Dejando el mouse por el titulo, cambiando a su color original...");
+        Elem.target.style.color = "black"
+    })
+
 })
