a2d4d4e5a1b1fe9a6e84a493153883001d2a2c38
Mike Andrade <hoodloom07@outlook.es>
2022-11-07 21:14:09 -0600

    14.10 Jquery events and bugs fixed

--- module14/jquery/index.js
+++ module14/jquery/index.js
@@ -3,4 +3,17 @@
  * Documentación de Jquery
  * https://api.jquery.com/
  */
+$(() => {
+//Etiqeut li oculta de hide
+//("li").hide()
 
+    //Etiqueta h1 oculta con hide
+    //$("h1").hide()
+
+    // referenciar ID es con " # "
+    $("#b-hide").hide()
+
+    // referenciar class con " . "
+    $(".b-show").hide()
+
+})
