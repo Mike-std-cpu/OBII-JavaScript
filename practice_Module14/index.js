d40694d843bb4cc648d45f557a126c4c41a7b19c
Mike Andrade <hoodloom07@outlook.es>
2022-11-11 00:27:15 -0600

    14.28 firt event and alert

---
+++ practice_Module14/index.js
@@ -0,0 +1,8 @@
+const boton = document.querySelector(".btn")
+
+console.log(boton);
+
+boton.addEventListener("click",()=>{
+    console.log("Precionaste el botn");
+    alert("¡Presionaste el boton! 😵‍💫")
+})
