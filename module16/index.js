e74d1b94c528939fbf9f259ef3b79cd608dd53d7
Mike Andrade <hoodloom07@outlook.es>
2022-11-18 23:46:53 -0600

    16.4 dragstart & dragend added in js and classList remove&add

--- module16/index.js
+++ module16/index.js
@@ -8,10 +8,11 @@ parrafos.forEach(parrafo => {
  parrafo.addEventListener("dragstart", () =>{ // cada que se arrastre cada uno de los elementos de la variable parrafo
     console.log("Iniciando el arrastre"); // Se imprioma por consola la accion
     console.log(`Estoy arrastrando el parrafo: ${parrafo.innerText}`); // InnerText agreg el terxto del parrafo
-    
+    parrafo.classList.add("Dragging") // Añadira una clase al elemento parrafo llamado "Dragging" cuando lo arrastre
+ })
+ parrafo.addEventListener("dragend", () =>{
+    console.log("Se termino el arrastre");
+    parrafo.classList.remove("Dragging")
  })
 
- parrafo.addEventListener("dragonEnd", () =>{
-    console.log("Se termino el arrastre");
- })
 });
