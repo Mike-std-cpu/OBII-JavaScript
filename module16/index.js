811c71c3765935bfc42e4a49f797e2033cb1e477
Mike Andrade <hoodloom07@outlook.es>
2022-11-18 23:38:45 -0600

    16.3.1 eventListener dragEnd

--- module16/index.js
+++ module16/index.js
@@ -7,5 +7,11 @@ console.log(parrafos); // Confirmacion de que se estan detectando los parrafs y 
 parrafos.forEach(parrafo => {
  parrafo.addEventListener("dragstart", () =>{ // cada que se arrastre cada uno de los elementos de la variable parrafo
     console.log("Iniciando el arrastre"); // Se imprioma por consola la accion
+    console.log(`Estoy arrastrando el parrafo: ${parrafo.innerText}`); // InnerText agreg el terxto del parrafo
+    
+ })
+
+ parrafo.addEventListener("dragonEnd", () =>{
+    console.log("Se termino el arrastre");
  })
 });
